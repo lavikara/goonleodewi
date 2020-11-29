b50771b5b00747a3db9ec7882e9d63dcdd603643
lavikara <karatemitayo@gmail.com>
2020-11-29 15:30:37 +0100

    feat: added placeholder to photo colunm, refactoed code

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -10,71 +10,88 @@
     </div>
     <div class="image-container container">
       <div class="photo-colunm">
-        <div v-for="photo in photos[0]" :key="photo.id" class="photo">
-          <div class="overlay"></div>
-          <img
-            :srcset="
-              `
+        <div class="placeholder" v-if="this.$store.state.loading">
+          <Placeholder v-for="i in 3" :key="i" />
+        </div>
+        <div v-else>
+          <div v-for="photo in photos[0]" :key="photo.id" class="photo">
+            <div class="overlay"></div>
+            <img
+              :srcset="
+                `
             ${photo.urls.raw}&w=1500&dpr=2 1500w,
             ${photo.urls.regular} 1080w,
             ${photo.urls.small} 400w
             `
-            "
-            sizes="(max-width: 600px) 100vw, 33.3vw "
-            :src="`${photo.urls.thumb}`"
-            loading="lazy"
-            alt="model image"
-            @click="openSinglePictureModal()"
-          />
-          <div class="details-container">
-            <h1>{{ photo.user.first_name }} {{ photo.user.last_name }}</h1>
-            <p>{{ photo.user.location }}</p>
+              "
+              sizes="(max-width: 600px) 100vw, 33.3vw "
+              :src="`${photo.urls.thumb}`"
+              loading="lazy"
+              alt="model image"
+              @click="openSinglePictureModal()"
+            />
+            <div class="details-container">
+              <h1>{{ photo.user.first_name }} {{ photo.user.last_name }}</h1>
+              <p>{{ photo.user.location }}</p>
+            </div>
           </div>
         </div>
       </div>
       <div class="photo-colunm">
-        <div v-for="photo in photos[1]" :key="photo.id" class="photo">
-          <div class="overlay"></div>
-          <img
-            :srcset="
-              `
+        <div class="placeholder" v-if="this.$store.state.loading">
+          <Placeholder v-for="i in 3" :key="i" />
+        </div>
+        <div v-else>
+          <div v-for="photo in photos[1]" :key="photo.id" class="photo">
+            <div class="overlay"></div>
+            <div>
+              <img
+                :srcset="
+                  `
             ${photo.urls.raw}&w=1500&dpr=2 1500w,
             ${photo.urls.regular} 1080w,
             ${photo.urls.small} 400w
             `
-            "
-            sizes="(max-width: 600px) 100vw, 33.3vw "
-            :src="`${photo.urls.thumb}`"
-            loading="lazy"
-            alt="model image"
-            @click="openSinglePictureModal()"
-          />
-          <div class="details-container">
-            <h1>{{ photo.user.first_name }} {{ photo.user.last_name }}</h1>
-            <p>{{ photo.user.location }}</p>
+                "
+                sizes="(max-width: 600px) 100vw, 33.3vw "
+                :src="`${photo.urls.thumb}`"
+                loading="lazy"
+                alt="model image"
+                @click="openSinglePictureModal()"
+              />
+              <div class="details-container">
+                <h1>{{ photo.user.first_name }} {{ photo.user.last_name }}</h1>
+                <p>{{ photo.user.location }}</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
       <div class="photo-colunm">
-        <div v-for="photo in photos[2]" :key="photo.id" class="photo">
-          <div class="overlay"></div>
-          <img
-            :srcset="
-              `
+        <div class="placeholder" v-if="this.$store.state.loading">
+          <Placeholder v-for="i in 3" :key="i" />
+        </div>
+        <div v-else>
+          <div v-for="photo in photos[2]" :key="photo.id" class="photo">
+            <div class="overlay"></div>
+            <img
+              :srcset="
+                `
             ${photo.urls.raw}&w=1500&dpr=2 1500w,
             ${photo.urls.regular} 1080w,
             ${photo.urls.small} 400w
             `
-            "
-            sizes="(max-width: 600px) 100vw, 33.3vw "
-            :src="`${photo.urls.thumb}`"
-            loading="lazy"
-            alt="model image"
-            @click="openSinglePictureModal()"
-          />
-          <div class="details-container">
-            <h1>{{ photo.user.first_name }} {{ photo.user.last_name }}</h1>
-            <p>{{ photo.user.location }}</p>
+              "
+              sizes="(max-width: 600px) 100vw, 33.3vw "
+              :src="`${photo.urls.thumb}`"
+              loading="lazy"
+              alt="model image"
+              @click="openSinglePictureModal()"
+            />
+            <div class="details-container">
+              <h1>{{ photo.user.first_name }} {{ photo.user.last_name }}</h1>
+              <p>{{ photo.user.location }}</p>
+            </div>
           </div>
         </div>
       </div>
@@ -83,25 +100,25 @@
 </template>
 
 <script>
+import Placeholder from "./Placeholder";
+
 export default {
   name: "Landing",
+  components: {
+    Placeholder,
+  },
+  data() {
+    return {};
+  },
+
+  created() {
+    this.$store.dispatch("getPhotos");
+  },
 
   computed: {
     photos() {
-      const totalPhotos = this.$store.state.photos;
-      const itemsInSubArray = 2;
-      const newArray = new Array(
-        Math.ceil(totalPhotos.length / itemsInSubArray)
-      )
-        .fill()
-        .map((_) => totalPhotos.splice(0, itemsInSubArray));
-      console.log(newArray);
-      return newArray;
+      return this.$store.state.photos;
     },
-  },
-
-  mounted() {
-    this.$store.dispatch("getPhotos");
   },
 };
 </script>
