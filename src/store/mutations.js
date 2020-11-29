f70391a8c3ed24ddf47f1f2b7d4981c1b27ced93
lavikara <karatemitayo@gmail.com>
2020-11-29 15:33:31 +0100

    chore: created loading mutation and refctored set photo mutation

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,7 +1,15 @@
 export const SET_PHOTOS = (state, photos) => {
-  state.photos = photos;
+  const itemsInSubArray = 3;
+  const newArray = new Array(Math.ceil(photos.length / itemsInSubArray))
+    .fill()
+    .map((_) => photos.splice(0, itemsInSubArray));
+  state.photos = newArray;
 };
 
 export const SET_ERROR = (state, error) => {
   state.error = error;
+};
+
+export const SET_LOADING = (state, loading) => {
+  state.loading = loading;
 };
