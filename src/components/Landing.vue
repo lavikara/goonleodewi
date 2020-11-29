6feadc5a9c93fd7f490ffa05ab5a90b296f1b436
lavikara <karatemitayo@gmail.com>
2020-11-29 07:16:41 +0100

    chore: rendered photos gotten from unsplash api using srcset 🥢 🥢

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -9,86 +9,74 @@
       />
     </div>
     <div class="image-container container">
-      <div>
-        <img
-          id="img1"
-          srcset="
-            https://res.cloudinary.com/dfsayl1xs/image/upload/v1598791584/calliah%27s%20touch/calliahsimg25x2_bd6z9q.jpg 960w,
-            https://res.cloudinary.com/dfsayl1xs/image/upload/v1598791582/calliah%27s%20touch/calliahsimg25x1_ijpgze.jpg 480w
-          "
-          sizes="(max-width: 600px) 100vw, 33.3vw "
-          src="https://res.cloudinary.com/dfsayl1xs/image/upload/v1598791580/calliah%27s%20touch/calliahsimg25-fallback_jrl6oh.jpg"
-          loading="lazy"
-          alt="model image"
-          @click="openSinglePictureModal()"
-        />
-        <img
-          srcset="
-            https://res.cloudinary.com/dfsayl1xs/image/upload/v1598789112/calliah%27s%20touch/calliahsimg12x4_tlzose.jpg 1920w,
-            https://res.cloudinary.com/dfsayl1xs/image/upload/v1598789111/calliah%27s%20touch/calliahsimg12x3_zxc2gn.jpg 1440w,
-            https://res.cloudinary.com/dfsayl1xs/image/upload/v1598789110/calliah%27s%20touch/calliahsimg12x2_ivgbwd.jpg  960w,
-            https://res.cloudinary.com/dfsayl1xs/image/upload/v1598789110/calliah%27s%20touch/calliahsimg12x1_xwnouh.jpg  480w
-          "
-          src="https://res.cloudinary.com/dfsayl1xs/image/upload/v1598789110/calliah%27s%20touch/calliahsimg12-fallback_jv3pam.jpg"
-          loading="lazy"
-          sizes="(max-width: 600px) 100vw, 33.3vw "
-          alt="model image"
-          @click="openSinglePictureModal()"
-        />
+      <div class="photo-colunm">
+        <div v-for="photo in photos[0]" :key="photo.id" class="photo">
+          <div class="overlay"></div>
+          <img
+            :srcset="
+              `
+            ${photo.urls.raw}&w=1500&dpr=2 1500w,
+            ${photo.urls.regular} 1080w,
+            ${photo.urls.small} 400w
+            `
+            "
+            sizes="(max-width: 600px) 100vw, 33.3vw "
+            :src="`${photo.urls.thumb}`"
+            loading="lazy"
+            alt="model image"
+            @click="openSinglePictureModal()"
+          />
+          <div class="details-container">
+            <h1>{{ photo.user.first_name }} {{ photo.user.last_name }}</h1>
+            <p>{{ photo.user.location }}</p>
+          </div>
+        </div>
       </div>
-      <div>
-        <img
-          id="img2"
-          srcset="
-            https://res.cloudinary.com/dfsayl1xs/image/upload/v1598785941/calliah%27s%20touch/calliahsimg1x3_crx7kv.jpg 1440w,
-            https://res.cloudinary.com/dfsayl1xs/image/upload/v1598785940/calliah%27s%20touch/calliahsimg1x2_k5mcz8.jpg  960w,
-            https://res.cloudinary.com/dfsayl1xs/image/upload/v1598785940/calliah%27s%20touch/calliahsimg1x1_sa8wvv.jpg  480w
-          "
-          sizes="(max-width: 600px) 100vw, 33.3vw "
-          src="https://res.cloudinary.com/dfsayl1xs/image/upload/v1598785940/calliah%27s%20touch/calliahsimg1-fallback_ipweki.jpg"
-          loading="lazy"
-          alt="model image"
-          @click="openSinglePictureModal()"
-        />
-        <img
-          id="img3"
-          srcset="
-            https://res.cloudinary.com/dfsayl1xs/image/upload/v1598790842/calliah%27s%20touch/calliahsimg19x2_m9xobo.jpg 960w,
-            https://res.cloudinary.com/dfsayl1xs/image/upload/v1598790841/calliah%27s%20touch/calliahsimg19x1_w3vukb.jpg 480w
-          "
-          src="https://res.cloudinary.com/dfsayl1xs/image/upload/v1598790841/calliah%27s%20touch/calliahsimg19-fallback_atln6i.jpg"
-          loading="lazy"
-          sizes="(max-width: 600px) 100vw, 33.3vw "
-          alt="model image"
-          @click="openSinglePictureModal()"
-        />
+      <div class="photo-colunm">
+        <div v-for="photo in photos[1]" :key="photo.id" class="photo">
+          <div class="overlay"></div>
+          <img
+            :srcset="
+              `
+            ${photo.urls.raw}&w=1500&dpr=2 1500w,
+            ${photo.urls.regular} 1080w,
+            ${photo.urls.small} 400w
+            `
+            "
+            sizes="(max-width: 600px) 100vw, 33.3vw "
+            :src="`${photo.urls.thumb}`"
+            loading="lazy"
+            alt="model image"
+            @click="openSinglePictureModal()"
+          />
+          <div class="details-container">
+            <h1>{{ photo.user.first_name }} {{ photo.user.last_name }}</h1>
+            <p>{{ photo.user.location }}</p>
+          </div>
+        </div>
       </div>
-      <div>
-        <img
-          id="img4"
-          srcset="
-            https://res.cloudinary.com/dfsayl1xs/image/upload/v1598790842/calliah%27s%20touch/calliahsimg18x2_os9evh.jpg 960w,
-            https://res.cloudinary.com/dfsayl1xs/image/upload/v1598790841/calliah%27s%20touch/calliahsimg18x1_qqggho.jpg 480w
-          "
-          src="https://res.cloudinary.com/dfsayl1xs/image/upload/v1598790841/calliah%27s%20touch/calliahsimg18-fallback_qbwqys.jpg"
-          loading="lazy"
-          sizes="(max-width: 600px) 100vw, 33.3vw "
-          alt="model image"
-          @click="openSinglePictureModal()"
-        />
-        <img
-          id="img2"
-          srcset="
-            https://res.cloudinary.com/dfsayl1xs/image/upload/v1598790223/calliah%27s%20touch/calliahsimg15x3_esweap.jpg 1440w,
-            https://res.cloudinary.com/dfsayl1xs/image/upload/v1598790221/calliah%27s%20touch/calliahsimg15x2_bdpzdz.jpg  960w,
-            https://res.cloudinary.com/dfsayl1xs/image/upload/v1598790221/calliah%27s%20touch/calliahsimg15x1_qm8sow.jpg  480w
-          "
-          sizes="(max-width: 600px) 100vw, 33.3vw "
-          src="https://res.cloudinary.com/dfsayl1xs/image/upload/v1598790221/calliah%27s%20touch/calliahsimg15-fallback_n9zbmu.jpg"
-          loading="lazy"
-          alt="model image"
-          @click="openSinglePictureModal()"
-        />
+      <div class="photo-colunm">
+        <div v-for="photo in photos[2]" :key="photo.id" class="photo">
+          <div class="overlay"></div>
+          <img
+            :srcset="
+              `
+            ${photo.urls.raw}&w=1500&dpr=2 1500w,
+            ${photo.urls.regular} 1080w,
+            ${photo.urls.small} 400w
+            `
+            "
+            sizes="(max-width: 600px) 100vw, 33.3vw "
+            :src="`${photo.urls.thumb}`"
+            loading="lazy"
+            alt="model image"
+            @click="openSinglePictureModal()"
+          />
+          <div class="details-container">
+            <h1>{{ photo.user.first_name }} {{ photo.user.last_name }}</h1>
+            <p>{{ photo.user.location }}</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -97,7 +85,22 @@
 <script>
 export default {
   name: "Landing",
-  props: {},
+
+  computed: {
+    photos() {
+      const totalPhotos = this.$store.state.photos;
+      const divider = 2;
+      const newArray = new Array(Math.ceil(totalPhotos.length / divider))
+        .fill()
+        .map((_) => totalPhotos.splice(0, divider));
+      console.log(newArray);
+      return newArray;
+    },
+  },
+
+  mounted() {
+    this.$store.dispatch("getPhotos");
+  },
 };
 </script>
 
