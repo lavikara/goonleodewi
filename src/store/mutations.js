fddb649861a397dbb551951c00887cbae1f8d780
lavikara <karatemitayo@gmail.com>
2020-11-30 12:19:12 +0100

    chore: refactor how items are destributed in the subarray, added mutations for query and photos

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,5 +1,12 @@
 export const SET_PHOTOS = (state, photos) => {
-  const itemsInSubArray = 3;
+  let itemsInSubArray = 0;
+  if (photos.length <= 6) {
+    itemsInSubArray = 2;
+  } else if (photos.length <= 3) {
+    itemsInSubArray = 1;
+  } else {
+    itemsInSubArray = 3;
+  }
   const newArray = new Array(Math.ceil(photos.length / itemsInSubArray))
     .fill()
     .map((_) => photos.splice(0, itemsInSubArray));
@@ -12,4 +19,12 @@ export const SET_ERROR = (state, error) => {
 
 export const SET_LOADING = (state, loading) => {
   state.loading = loading;
+};
+
+export const SET_SEARCHQUERY = (state, query) => {
+  state.searchQuery = query;
+};
+
+export const RESET_PHOTOS = (state) => {
+  state.photos = [];
 };
